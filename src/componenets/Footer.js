b6254068fe23ae5599e66535e056970f6db1ae77
polamounir103 <polamounir103@gmail.com>
2024-01-29 04:36:43 +0200

    first commit

--- src/componenets/Footer.js
+++ src/componenets/Footer.js
@@ -3,10 +3,10 @@ import logo from "../asstes/images/D.jpg";
 function Footer() {
   return (
     <div>
-      <footer className="flex h-fit justify-around items-center p-16 bg-black text-white">
+      <footer className="flex h-fit justify-around items-center p-16 bg-black text-yellow-400">
         <img src={logo} className="h-16 w-16 rounded-full"/>
         <div className="flex flex-col">
-          <h2>JOhn MOunir</h2>
+          <h2 className=" ">JOhn MOunir</h2>
           <h3>Frontend Developer</h3>
         </div>
         <div>
